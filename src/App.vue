08c0ba7fcec2728d72fb7643af1200aec2f84e17
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-07-13 12:10:27 +0800

    加载工具完成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>黑马头条</div>
+  <div>
+    <div>黑马头条</div>
+    <span class="toutiao toutiao-dianzan"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <div class="login"></div>
+
+    <div style="background: gold; height: 200px"></div>
+  </div>
 </template>
 
 <script>
@@ -11,6 +23,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.login{
+  width: 694px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入vant所有组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 引入字体图标的样式
+import '@/assets/fonts/iconfont.css'
+// 引入flexble
+import 'amfe-flexible/index.min.js'
 
 Vue.config.productionTip = false
+
+// 使用vant
+Vue.use(Vant)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
